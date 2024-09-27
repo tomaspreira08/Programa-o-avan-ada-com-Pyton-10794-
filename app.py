ff02b25f2c0106a6268759f45055321a2774b16d
tomaspreira08 <tomaspereira04062008@gmail.com>
2024-09-27 13:08:25 +0100

    Página Inicial

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Olá, Mundo!"
+def index():
+    return render_template('index.html')
+
+@app.route('/administracao_redes_locais')
+def administracao_redes_locais():
+    return render_template('administracao_redes_locais.html')
+
+@app.route('/processamento_computacional')
+def processamento_computacional():
+    return render_template('processamento_computacional.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
