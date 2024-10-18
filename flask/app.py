ac343d06a37a84db38cd9b5c8f417aea5daed554
tomaspreira08 <tomaspereira04062008@gmail.com>
2024-10-18 14:30:33 +0100

    styles e 1º topico

--- flask/app.py
+++ flask/app.py
@@ -14,6 +14,22 @@ def administracao_redes_locais():
 def processamento_computacional():
     return render_template('processamento_computacional.html')
 
+@app.route('/c_e_c++')
+def c_e_c():
+    return render_template('c e c++.html')
+
+@app.route('/administração_de_bd')
+def administração_de_bd():
+    return render_template('admin. de base de dados.html')
+
+@app.route('/JAVA')
+def JAVA():
+    return render_template('prog com java.html')
+
+@app.route('/Python')
+def Python():
+    return render_template('prog com py.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
